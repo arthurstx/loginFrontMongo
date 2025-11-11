4dfe277ebef41133c9170fb3dea8c96b56f153a6
ArthurG1t <arthurteixeira.git@gmail.com>
2025-11-11 14:39:51 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './hooks/useAuth';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import React from 'react';
-import { ProtectedRoute } from './components/layout/ProtectedRoute';
-import FindPersonPage from './pages/FindPersonPage';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider, useAuth } from "./hooks/useAuth";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import React from "react";
+import { ProtectedRoute } from "./components/layout/ProtectedRoute";
+import FindPersonPage from "./pages/FindPersonPage";
 
 function AppRoutes(): React.JSX.Element {
   const { isAuthenticated } = useAuth();
@@ -13,17 +13,25 @@ function AppRoutes(): React.JSX.Element {
   return (
     <Routes>
       {/* Redireciona a raiz para o login ou dashboard */}
-      <Route path="/" element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} replace />} />
+      <Route
+        path="/"
+        element={
+          <Navigate to={isAuthenticated ? "/find-person" : "/login"} replace />
+        }
+      />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      
+
       {/* Rota Protegida */}
-      <Route path="/find-person" element={
-        <ProtectedRoute>
-          <FindPersonPage />
-        </ProtectedRoute>
-      } />
-      
+      <Route
+        path="/find-person"
+        element={
+          <ProtectedRoute>
+            <FindPersonPage />
+          </ProtectedRoute>
+        }
+      />
+
       <Route path="*" element={<h1>404 - Página Não Encontrada</h1>} />
     </Routes>
   );
